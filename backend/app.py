b82cc79c7369446ebf7da74630e8c342ab46c718
fabio thomaz <fabio_thomaz@live.com>
2025-01-11 08:51:50 -0400

    aplicativo conectado ao banco de ddados

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,32 @@
 from flask import Flask
+from sqlalchemy import text  # Importa text para criar consultas SQL
+from extensions import db
 from add_url_rtsp import add_url_rtsp
 
+
+# create the Flask app
+
 app = Flask(__name__)
+
+# configure the MariaDb database
+app.config["SQLALCHEMY_DATABASE_URI"] = "mariadb+mariadbconnector://fabio:root@127.0.0.1:3306/rtsp_conn_api"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def test_db_connection():
+    try:
+        with app.app_context():
+            # Obtém uma conexão com o banco de dados
+            with db.engine.connect() as connection:
+                # Cria a consulta usando sqlalchemy.text
+                result = connection.execute(text("SELECT 1"))
+                for row in result:
+                    print(f"Conexão bem-sucedida ao banco de dados! Resultado: {row[0]}")
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+        return False
+    return True
+
+test_db_connection()
 
 app.register_blueprint(add_url_rtsp, url_prefix='/add_url_rtsp')
